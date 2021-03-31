dc8e868fe82b8b25099a4285a8ed6f1fd0c71b72
Bruno Marques Caroba <cocacola_bruno@hotmail.com>
2021-03-30 22:04:02 -0300

    Add event listeners on singIn buttons

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
-	console.log('TEST');
+
+	let signInButtons = document.getElementsByClassName("signin-button");
+
+	for(let i = 0; i < signInButtons.length; i++)
+		signInButtons.item(i).addEventListener("click", onLoginClick);
+
 });
+
+function onLoginClick() {
+	let doc = document.querySelector(".login-modal");
+
+	doc.classList.add("login-modal-active");
+}
