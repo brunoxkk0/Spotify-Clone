c6736a6d6177e5eb0d01a6fdd074226426742fac
Bruno Marques Caroba <cocacola_bruno@hotmail.com>
2021-04-02 23:07:09 -0300

    Implementado lógica base para o sistema de login.

--- js/main.js
+++ js/main.js
@@ -3,13 +3,19 @@ document.addEventListener('DOMContentLoaded', function(){
 	let signInButtons = document.getElementsByClassName("signin-button");
 	let loginModalBackground = document.querySelector(".login-modal-background");
 	let loginModalClose = document.querySelector(".close-button");
+	let loginForm = document.querySelector(".login-form");
 
 	loginModalBackground.addEventListener("click", disableModal);
 	loginModalClose.addEventListener("click", disableModal);
 
 	for(let i = 0; i < signInButtons.length; i++)
 		signInButtons.item(i).addEventListener("click", activeModal);
-	
+
+	loginForm.addEventListener("submit", function (event){
+		event.preventDefault();
+		onSignInRequest();
+	})
+
 });
 
 function activeModal() {
@@ -30,4 +36,36 @@ function disableModal(){
 	body.classList.remove("disable-scroll");
 	doc.classList.remove("login-modal-active");
 	modalBackground.classList.remove("login-m-background-active");
+}
+
+function onSignInRequest(){
+
+	let usernameField = document.querySelector(".login-username-field");
+	let passwordField = document.querySelector(".login-password-field");
+
+	if(usernameField.value != null && passwordField.value != null){
+		requestLogin(usernameField.value, passwordField.value, function (token){
+			console.log("Token encontrado: " + token);
+		})
+	}
+}
+
+function requestLogin(email, password, callback){
+
+	let request = new XMLHttpRequest();
+
+	request.open("POST", "https://reqres.in/api/login", true);
+	request.setRequestHeader("Content-Type", "application/json; charset=utf-8")
+	request.onreadystatechange = function () {
+
+		if(request.readyState === 4 && request.status === 200){
+			callback(JSON.parse(request.responseText).token);
+		}
+
+	};
+
+	request.send(JSON.stringify({
+		email: email,
+		password: password
+	}));
 }
