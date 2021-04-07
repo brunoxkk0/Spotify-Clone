5befd1ffe394546910d6137b397b1bcb4515366c
Bruno Marques Caroba <cocacola_bruno@hotmail.com>
2021-04-06 22:33:25 -0300

    Implementado lógica de autentificação com a API do Spotify

--- js/main.js
+++ js/main.js
@@ -52,6 +52,7 @@ function onSignInRequest(){
 				setErrorFields();
 			}else{
 				localStorage.setItem("loginToken",response.token)
+				checkSpotifyAuth()
 			}
 		});
 	}
@@ -90,4 +91,96 @@ function setErrorFields(){
 	}else{
 		passwordField.classList.remove("error-field");
 	}
+}
+
+function requestLogin(email, password, callback){
+
+	let request = new XMLHttpRequest();
+
+	request.open("POST", "https://reqres.in/api/login", true);
+	request.setRequestHeader("Content-Type", "application/json; charset=utf-8")
+	request.onreadystatechange = function () {
+
+		if(request.readyState !== 4){
+			return;
+		}
+
+		if(request.status === 200){
+			callback(JSON.parse(request.responseText).token);
+		}
+
+	};
+
+	request.send(JSON.stringify({
+		email: email,
+		password: password
+	}));
+}
+
+function checkSpotifyAuth(){
+
+	let cached_auth_credentials = localStorage.getItem('auth_credentials');
+
+	let auth_data = {
+		credentials_validity: undefined
+	}
+
+	if(cached_auth_credentials !== null){
+		auth_data = JSON.parse(cached_auth_credentials);
+	}
+
+	let expire_time = new Date().getTime();
+
+	if(auth_data.credentials_validity >= expire_time){
+		return auth_data;
+	}
+
+	let data = new URLSearchParams(window.location.search);
+	let access_token = data.get("access_token");
+
+	if(access_token != null){
+
+		let last_auth = localStorage.getItem("last_auth");
+
+		if(last_auth != null && new Date().getTime() < last_auth + (3600000)){
+
+			auth_data = {
+				credentials_validity: last_auth + 3600000,
+				access_token: access_token,
+				token_type: data.get("token_type")
+			}
+
+			localStorage.setItem("auth_credentials", JSON.stringify(auth_data));
+
+			return auth_data;
+		}
+
+	}
+
+	callSpotifyAuth();
+
+}
+
+function callSpotifyAuth(){
+
+	let Scopes = [
+		"user-read-private"
+	]
+
+	let Params = {
+		client_id: "16b2643340d04892ab30d20bfe554546",
+		response_type: "token",
+		redirect_uri: "https://brunoxkk0.github.io/Spotify-Clone/",
+		scope: encodeURI(Scopes.join(' '))
+	}
+
+	let auth_url = "https://accounts.spotify.com/authorize?" + Object.keys(Params).map(key => key + '=' + Params[key]).join('&');
+
+	let width 	= 450
+	let height 	= 730
+	let left 	= (screen.width / 2) - (width / 2)
+	let top 	= (screen.height / 2) - (height / 2);
+
+	localStorage.setItem('last_auth', "" + new Date().getTime());
+	window.open(auth_url, '_self', 'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left );
 }
